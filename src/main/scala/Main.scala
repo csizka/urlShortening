b33db9279c18b9e9e68a3f1f9a902bf59a125af6
csizma <csizmadia.kata@gmail.com>
2024-09-24 22:50:50 +0200

    used preparedstatements, simplified resource - mgmnt

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,7 @@
 import java.sql.*
-import scala.io.StdIn
-import scala.util.{Try, Success, Failure}
-
 
 def printUrlHandlePairs(resSet: ResultSet): Unit = {
+  println("printing handle - URL pairs:")
   while (resSet.next()) {
     val url = resSet.getString("url")
     val handle = resSet.getString("handle")
@@ -12,58 +10,82 @@ def printUrlHandlePairs(resSet: ResultSet): Unit = {
   println("all requested values printed")
 }
 
-def insertRows(statement: Statement, rows: Seq[(String, String)], table: String): Unit = {
+def insertRows(statement: PreparedStatement, rows: Seq[(String, String)]): Unit = {
   val count = rows.foldLeft (0) { case (curCount, (handle, url)) => 
-      val insertQuery = s"INSERT INTO ${table} VALUES ('${handle}', '${url}') ON CONFLICT (handle) DO NOTHING;"
-      statement.executeUpdate(insertQuery) + curCount 
+      statement.setString(1, handle)
+      statement.setString(2, url)
+      statement.executeUpdate() + curCount 
   }
-  println(s"${count} record(s) inserted into table '${table}'")
+  println(s"${count} record(s) inserted to the table.")
 }
 
-def bracket[A, B](acquire: => A)(use: A => B)(release: A => Unit): Try[B] = {
-  val resource = Try(acquire)
-  val result = resource.map(use)
-  resource.map(release)
-  result
+def connectToDB(): Connection = {
+  val dbName = "mydb"
+  val host = "localhost"
+  val port = "5432"
+  val url = s"jdbc:postgresql://${host}:${port}/${dbName}"
+
+  val conn = DriverManager.getConnection(url)
+  println("connection created")
+  conn
+}
+
+def closeDBConn(conn: Connection): Unit = {
+  conn.close()
+  println("connection closed")
+}
+
+def closeResSet(resSet: ResultSet): Unit = {
+  resSet.close()
+  println("result set closed")
+}
+
+def closeStatement(stmnt: PreparedStatement): Unit = {
+  stmnt.close()
+  println("statement closed")
+}
+
+def closeStatement(stmnt: Statement): Unit = {
+  stmnt.close()
+  println("statement closed")
 }
 
 def printRecords(): Unit = {
-  val dbName = "mydb"
-  val host = "localhost"
-  val port = "5432"
+  val conn = connectToDB()
   val tableName = "url"
-  val url = s"jdbc:postgresql://${host}:${port}/${dbName}"
+  val stmnt = conn.prepareStatement(s"SELECT * FROM ${tableName};")
+  println("statement created")
 
-  bracket(DriverManager.getConnection(url)) { conn => 
-    println("connection created")
-    bracket(conn.createStatement()) { stmnt => 
-      println("statement created")
-      bracket(stmnt.executeQuery(s"SELECT * FROM ${tableName};")) { resSet => 
-        println("result set created")
-        printUrlHandlePairs(resSet)
-      } { rs => rs.close(); println("result set closed") }
-    } { stmnt => stmnt.close(); println("statement closed") }
-  } { conn => conn.close(); println("connection closed") }
+  val resSet = stmnt.executeQuery()
+  println("result set created")
+
+  printUrlHandlePairs(resSet)
+    
+  closeResSet(resSet)
+  closeStatement(stmnt)    
+  closeDBConn(conn)
 }
 
-def insertAndPrintRows(rows: Seq[(String, String)] = Seq(("test1", "test1"))): Unit = {
-  val dbName = "mydb"
+def insertAndPrintRows(rows: Seq[(String, String)] = Seq(("test2", "test2"))): Unit = {
   val tableName = "url"
-  val host = "localhost"
-  val port = "5432"
-  val url = s"jdbc:postgresql://${host}:${port}/${dbName}"
+  val conn = connectToDB()
 
-  bracket(DriverManager.getConnection(url)){ conn =>
-    println("connection created")
-    bracket(conn.createStatement()) { stmnt =>
-      println("statement created")
-      insertRows(stmnt, rows, tableName)
-      bracket(stmnt.executeQuery(s"SELECT * FROM ${tableName};")) { resSet =>
-        println("result set created")
-        printUrlHandlePairs(resSet)
-      } {resSet => resSet.close()}
-    } {stmnt => stmnt.close(); println("statement closed")}
-  } {conn => conn.close(); println("connection closed")}
+  val insertStmnt = conn.prepareStatement(s"INSERT INTO ${tableName} VALUES (?, ?) ON CONFLICT (handle) DO NOTHING;")
+  println("statement created")
+
+  insertRows(insertStmnt, rows)
+  closeStatement(insertStmnt)
+  
+  val selectAllStmnt = conn.prepareStatement(s"SELECT * FROM ${tableName};")
+  println("statement created")
+  val resSet = selectAllStmnt.executeQuery()
+  println("result set created")
+
+  printUrlHandlePairs(resSet)
+
+  closeResSet(resSet)
+  closeStatement(selectAllStmnt) 
+  closeDBConn(conn)
 }
 
 def insertAndPrintRows(handle: String, url: String): Unit = {
@@ -72,5 +94,5 @@ def insertAndPrintRows(handle: String, url: String): Unit = {
 
 @main 
 def runFns(): Unit = {
-insertAndPrintRows()
+  insertAndPrintRows()
 }
